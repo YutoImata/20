6942dec0a7c9ad7fb082e7142b0ce7a21a7578fc
YutoImata <yuto.imata.wine@gamil.com>
2025-03-12 00:42:20 +0900

    feat: add overall design improvements

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,12 @@
-function goToPage(url) {
-    window.location.href = url;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".celebration-btn:not(.locked)");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            const url = this.getAttribute("data-url");
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    });
+});
--- memo.md
+++ memo.md
@@ -1,3 +1,8 @@
 # 何をお祝いするか
 ・iPad
 ・
+
+### その他
+・リンク
+↓
+https://yutoimata.github.io/20/

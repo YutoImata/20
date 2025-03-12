51643c4b6a3a802d6f0c96f77c2e1ae209f7d000
YutoImata <yuto.imata.wine@gamil.com>
2025-03-13 02:25:21 +0900

    feat: add passward system

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,17 @@
 // JS/welcome.js
-function goToNextPage() {
-    // メッセージとボタンを非表示にする
-    document.getElementById('welcome-screen').style.display = 'none';
-    
-    // 次のページへ遷移
-    window.location.href = 'HTML/home.html'; // 次のページのURLを指定
+function checkPassword() {
+    const password = document.getElementById('password').value;
+    const correctPassword = '9118'; // ここに正しいパスワードを設定
+
+    // パスワードが正しいか確認
+    if (password === correctPassword) {
+        // メッセージと入力フォームを非表示にする
+        document.getElementById('welcome-screen').style.display = 'none';
+        
+        // 次のページへ遷移
+        window.location.href = 'HTML/home.html'; // 次のページのURLを指定
+    } else {
+        // エラーメッセージを表示
+        document.getElementById('error-message').style.display = 'block';
+    }
 }
